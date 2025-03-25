ce4fa30c714f4d4ab7120b7881f72dccaa014720
lukeforte40 <lukeforte40@gmail.com>
2025-03-24 21:40:05 -0400

    fixed input update issue

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -5,14 +5,27 @@ import { InputCheckboxComponent } from "./types"
 export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
   const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
   const [isChecked, setIsChecked] = useState(checked);
-  const handleLabelClick = () =>{
-    setIsChecked(!isChecked);
-  }
+
+  const handleChange = () => {
+    const newCheckedState = !isChecked;
+    setIsChecked(newCheckedState);
+
+    if (onChange) {
+      onChange(newCheckedState); 
+    }
+  };
+
+  const handleLabelClick = (e: React.MouseEvent<HTMLLabelElement>) => {
+    if (!disabled) {
+      handleChange();
+    }
+    e.preventDefault();
+  };
 
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
-      onClick={handleLabelClick}
+      onClick={(e) => handleLabelClick(e)}
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": isChecked,
           "RampInputCheckbox--label-disabled": disabled,
@@ -24,7 +37,7 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
         className="RampInputCheckbox--input"
         checked={isChecked}
         disabled={disabled}
-        onChange={() => onChange(!checked)}
+        onChange={() => onChange(!isChecked)}
       />
     </div>
   )
